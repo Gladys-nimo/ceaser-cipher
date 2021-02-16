82f2db76d6af9f70a3b2a73a99d19a3781c0c3d6
Gladys-nimo <wkanyora10@gmail.com>
2021-02-16 12:43:02 -0800

    y tests are running and I changed some dependancies

--- src/test/java/models/CipherTest.java
+++ src/test/java/models/CipherTest.java
@@ -1,13 +1,15 @@
 package models;
 
-import org.junit.*;
-import static org.junit.Assert.*;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CipherTest {
     @Test
     public void newCipher_instantiatesCorrectly() {
         Cipher testCipher = new Cipher("a",2);
-        assertEquals("a",testCipher instanceof Cipher);
+        assertEquals(true,testCipher instanceof Cipher);
     }
 
     @Test
