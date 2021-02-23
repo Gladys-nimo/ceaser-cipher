24f0f6b042b86d68e4ec1106465fc7410acf192c
Gladys-nimo <wkanyora10@gmail.com>
2021-02-23 10:45:12 -0800

    made some changes

--- src/test/java/models/CipherTest.java
+++ src/test/java/models/CipherTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CipherTest {
+
     @Test
     public void newCipher_instantiatesCorrectly() {
         Cipher testCipher = new Cipher("a",2);
